15f488e1c7712da734aedcbb9bd65c46a1fc2114
jorgemrDev <black.spade.666@gmail.com>
2021-04-12 16:28:22 -0500

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 const API_URL = "http://localhost:6107/";
 const IMAGE_BASE_URL = "http://localhost:6105/StaticFiles/";
+const API_URL = "TOKEN";
 
-export { API_URL, IMAGE_BASE_URL };
+export { API_URL, IMAGE_BASE_URL, API_URL };
